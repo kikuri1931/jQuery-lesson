dd04b07317fc7ca441e9cc0bdb0391c7fec82572
kikuri1931 <ys1945050mite.blue@gmail.com>
2020-08-21 12:30:14 +0900

    [Finish] 6章 「イベントを検出しよう」

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 $(function(){
-	$('.box1').slideDown(3000,function(){
-		$('.box1').css({
-			'background-color': '#0000FF',
-			'height': '100px'
-		}).slideUp(3000);
+	$('.box1').on('click', function(){
+		$('.box1').addClass('box1-ext');
+	});
+	$('.box1').mouseout(function(){
+		$('.box1').removeClass('box1-ext');
 	});
 });
 	
